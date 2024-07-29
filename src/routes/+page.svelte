feb6ca96533a7fac3727d609e60d938480b458ac
Gray <graythecolor@proton.me>
2024-07-28 18:18:13 -0700

    basic stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { browser } from "$app/environment";
+    import { goto } from "$app/navigation";
+import Navbar from "./Navbar.svelte";
+</script>
+
+<svelte:head>
+    <title>lemma</title>
+</svelte:head>
+
+
+<div class="flex flex-col h-screen justify-between">
+    <header>
+        <Navbar/>
+    </header>
+    <main>
+        <div class="grid justify-items-center">
+            <div class="mt-10">
+                <h1
+                    class="scroll-m-20 text-4xl font-extrabold lg:text-5xl"
+                >
+                    A federated alternative to Facebook Marketplace.
+                </h1>
+            </div>
+            <div class="mt-5">
+                    <button class="btn btn-primary" on:click={()=>{if(browser){goto("/signup")}}}>Sign up</button>
+                    <button class="btn btn-neutral">Learn more</button>
+                
+            </div>
+        </div>     
+    </main>
+    <footer class="text-center mb-5">
+        <a href="https://github.com/letlovewin/lemma">Github Repo</a>
+    </footer>
+</div>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,8 @@
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <p class="btn btn-ghost text-xl">lemma</p>
+    </div>
+    <div class="flex-none">
+        <button class="btn btn-primary"> Sign in </button>
+    </div>
+</div>
