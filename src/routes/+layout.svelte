0c2a3df228d3c9dbc2beab3299458fd18f9892fe
Gray <graythecolor@proton.me>
2024-07-29 11:00:51 -0700

    converted to typescript, added supabase authguards

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,40 @@
 <script>
-import "../app.css";
-import { browser } from "$app/environment";
-import { goto } from "$app/navigation";
+    import "../app.css";
+    import { browser } from "$app/environment";
+    import { goto, invalidate } from "$app/navigation";
+    import { onMount } from "svelte";
+
+    export let data;
+    $: ({ session, supabase } = data);
+
+    onMount(() => {
+        const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+            if (newSession?.expires_at !== session?.expires_at) {
+                invalidate("supabase:auth");
+            }
+        });
+
+        return () => data.subscription.unsubscribe();
+    });
 </script>
 
 <div class="flex flex-col h-screen justify-between">
     <header>
         <div class="navbar bg-base-100">
             <div class="flex-1">
-                <a href="/" class="btn btn-ghost text-xl">
-                    lemma
-                </a>
+                <a href="/" class="btn btn-ghost text-xl"> lemma </a>
             </div>
             <div class="flex-none">
-                <button class="btn btn-primary btn-sm" on:click={()=>{if(browser){goto("/signin")}}}> Sign in </button>
-                
+                <button
+                    class="btn btn-primary btn-sm"
+                    on:click={() => {
+                        if (browser) {
+                            goto("/signin");
+                        }
+                    }}
+                >
+                    Sign in
+                </button>
             </div>
         </div>
     </header>
@@ -22,6 +42,7 @@ import { goto } from "$app/navigation";
         <slot></slot>
     </main>
     <footer class="text-center mb-5">
-        <a href="https://github.com/letlovewin/lemma">Github Repo</a> | <a href="https://fediverse.info/">Fediverse</a>
+        <a href="https://github.com/letlovewin/lemma">Github Repo</a> |
+        <a href="https://fediverse.info/">Fediverse</a>
     </footer>
 </div>
