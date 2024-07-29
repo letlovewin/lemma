d16130e6808baef1e53f0d8bc3102725f737728c
Gray <graythecolor@proton.me>
2024-07-29 07:15:33 -0700

    got the environment variables figured out

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script>
     import { browser } from "$app/environment";
     import { goto } from "$app/navigation";
+
     import Footer from "./Footer.svelte";
     import HomeNotAuthenticated from "./HomeNotAuthenticated.svelte";
     import Navbar from "./Navbar.svelte";
 
     const isSignedIn = 0;
     import {createClient} from "@supabase/supabase-js"
-    import {PUBLIC_SUPABASE_KEY} from '$env/static/public'
-
-    const supabaseUrl = "https://uqrzveflwowlasogcpfv.supabase.co";
-    const supabaseKey = PUBLIC_SUPABASE_KEY;
+    
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
     const supabase = createClient(supabaseUrl, supabaseKey);
 
 </script>
